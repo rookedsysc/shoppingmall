3f004719fef81d9126e5b272496c2a4f54452330
rookedsysc <rookedsysc36@gmail.com>
2023-12-25 06:00:48 +0900

    hotfix Entity Sequence not found Error 수정 http://rookedsysc.vercel.app/java-and-spring/project/shoppingmall\#item-seq-not-found

--- src/main/java/org/example/shoppingmall/item/domain/entity/ItemEntity.java
+++ src/main/java/org/example/shoppingmall/item/domain/entity/ItemEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public class ItemEntity {
   @Id
   @Column
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Column(nullable = false, length = 50)
