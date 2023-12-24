07dc97044c864040deddfbe895db4415780da879
rookedsysc <rookedsysc36@gmail.com>
2023-12-24 18:29:34 +0900

    feat create project

---
+++ src/test/java/org/example/shoppingmall/ShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
